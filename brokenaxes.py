127c960ad430e4a634557b7c3354a1192921310c
Ben Dichter <ben.dichter@gmail.com>
2017-04-12 21:49:47 -0700

    refactor spines and ticks and fix README reference

--- brokenaxes.py
+++ brokenaxes.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+import matplotlib.ticker as ticker
 
 
 class BrokenAxes:
-    def __init__(self, xlims=None, ylims=None, d=.015, tilt=None, subplot_spec=None, fig=None, *args, **kwargs):
+    def __init__(self, xlims=None, ylims=None, d=.015, tilt=None,
+    subplot_spec=None, fig=None, *args, **kwargs):
 
         if fig is None:
             fig = plt.figure()
@@ -40,8 +42,6 @@ class BrokenAxes:
             self.axs.append(ax)
         fig.add_subplot(self.big_ax)
 
-        d_kwargs = dict(transform=fig.transFigure, color='k', clip_on=False)
-
         bounds = self.big_ax.get_position().bounds
         if tilt is None:
             tilt = bounds[2] / bounds[3]
@@ -51,16 +51,20 @@ class BrokenAxes:
                 ax.set_ylim(ylims[::-1][i//ncols])
             if xlims is not None:
                 ax.set_xlim(xlims[i % ncols])
-            ax.spines['top'].set_visible(False)
-            ax.spines['right'].set_visible(False)
-            if not ax.is_last_row():
-                ax.spines['bottom'].set_visible(False)
-                ax.set_xticks([])
-            elif d:
+
+        self.standardize_ticks()
+        if d:
+            self.draw_diags(d, tilt, fig)
+        self.set_spines()
+
+
+    def draw_diags(self, d, tilt, fig):
+        d_kwargs = dict(transform=fig.transFigure, color='k', clip_on=False)
+        for ax in self.axs:
+            if ax.is_last_row():
                 if not ax.is_last_col():
                     xpos = ax.get_position().bounds[0] + ax.get_position().bounds[2]
                     ypos = ax.get_position().bounds[1]
-
                     ax.plot((xpos - d, xpos + d), (ypos - tilt * d,  ypos + tilt * d), **d_kwargs)
 
                 if not ax.is_first_col():
@@ -68,11 +72,7 @@ class BrokenAxes:
                     ypos = ax.get_position().bounds[1]
                     ax.plot((xpos - d, xpos + d), (ypos - tilt * d, ypos + tilt * d), **d_kwargs)
 
-            if not ax.is_first_col():
-                ax.spines['left'].set_visible(False)
-                ax.set_yticks([])
-
-            elif d:
+            if ax.is_first_col():
                 if not ax.is_first_row():
                     xpos = ax.get_position().bounds[0]
                     ypos = ax.get_position().bounds[1] + ax.get_position().bounds[3]
@@ -83,8 +83,43 @@ class BrokenAxes:
                     ypos = ax.get_position().bounds[1]
                     ax.plot((xpos - d, xpos + d), (ypos - tilt * d, ypos + tilt * d), **d_kwargs)
 
+
+    def set_spines(self):
+        for ax in self.axs:
+            ax.spines['top'].set_visible(False)
+            ax.xaxis.tick_bottom()
+            ax.spines['right'].set_visible(False)
+            ax.yaxis.tick_left()
+            if not ax.is_last_row():
+                ax.spines['bottom'].set_visible(False)
+                ax.set_xticks([])
+            if not ax.is_first_col():
+                ax.spines['left'].set_visible(False)
+                ax.set_yticks([])
+
+
+    def standardize_ticks(self):
+        xbases, ybases = [], []
+        for ax in self.axs:
+            if ax.is_first_col():
+                ybases.append(ax.yaxis.get_ticklocs()[1] - ax.yaxis.get_ticklocs()[0])
+            if ax.is_last_row():
+                xbases.append(ax.xaxis.get_ticklocs()[1] - ax.xaxis.get_ticklocs()[0])
+
+        for ax in self.axs:
+            if ax.is_first_col():
+                ax.yaxis.set_major_locator(ticker.MultipleLocator(max(ybases)))
+            if ax.is_last_row():
+                ax.xaxis.set_major_locator(ticker.MultipleLocator(max(xbases)))
+
     def plot(self, *args, **kwargs):
-        [ax.plot(*args, **kwargs) for ax in self.axs]
+        for ax in self.axs:
+            ax.xaxis.set_major_locator(ticker.AutoLocator())
+            ax.yaxis.set_major_locator(ticker.AutoLocator())
+            ax.plot(*args, **kwargs)
+
+        self.standardize_ticks()
+        self.set_spines()
 
     def set_xlabel(self, *args, labelpad=20, **kwargs):
         self.big_ax.set_xlabel(*args, labelpad=labelpad, **kwargs)
