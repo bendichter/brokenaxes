625ab3e3b288ca2ec1be8266f763d6d7dbb07ed9
webersa <samuel.weber@univ-grenoble-alpes.fr>
2019-04-11 17:28:09 +0200

    add sphinx.ext.napoleon for nice formating

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
